549dfa6963d494c559ed720d8b6c87a91eff7c0f
YiFengHu <yuiop74182006@gmail.com>
2015-04-12 00:57:04 +0800

    [Youtube] 1. Add Youtube API function (YouTubePlayer) 2. Finish hand, leg and body activity

---
+++ Shaking/src/com/nptu/dse/shaking/activity/HandSportActivity.java
@@ -0,0 +1,59 @@
+package com.nptu.dse.shaking.activity;
+
+import android.os.Bundle;
+
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+import com.google.android.youtube.player.YouTubePlayer.Provider;
+import com.nptu.dse.shaking.R;
+import com.nptu.dse.shaking.resource.DeveloperKey;
+
+public class HandSportActivity extends YouTubeFailureRecoveryActivity {
+
+	private static final String TAG = HandSportActivity.class.getSimpleName();
+
+	private static final String videoId = "ALAaIcLTS4Y";
+	private YouTubePlayerView youtubePlayerView = null;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_hand_sport);
+
+		youtubePlayerView = (YouTubePlayerView) findViewById(R.id.handSport_youtubeView);
+		youtubePlayerView.initialize(DeveloperKey.DEVELOPER_KEY, this);
+	}
+
+	@Override
+	public void onInitializationSuccess(YouTubePlayer.Provider provider,
+			YouTubePlayer player, boolean wasRestored) {
+		if (!wasRestored) {
+			player.cueVideo(videoId);
+			player.play();
+		}
+	}
+
+	@Override
+	protected Provider getYouTubePlayerProvider() {
+		return (YouTubePlayerView) findViewById(R.id.handSport_youtubeView);
+	}
+
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+	}
+
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+	}
+
+	@Override
+	protected void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
+	}
+
+}
--- Shaking/src/com/nptu/dse/shaking/activity/SelectSportActivity.java
+++ Shaking/src/com/nptu/dse/shaking/activity/SelectSportActivity.java
@@ -3,17 +3,36 @@ package com.nptu.dse.shaking.activity;
 import com.nptu.dse.shaking.R;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class SelectSportActivity extends Activity{
+public class SelectSportActivity extends Activity implements OnClickListener {
+
+	private static final String TAG = SelectSportActivity.class.getSimpleName();
+
+	private Button handButton = null;
+	private Button legButton = null;
+	private Button bodyButton = null;
+	
+	private Intent intent = null;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_select);
-		
-	}
+		setContentView(R.layout.activity_select_video);
 
+		handButton = (Button) findViewById(R.id.select_handTextView);
+		legButton = (Button) findViewById(R.id.select_legTextView);
+		bodyButton = (Button) findViewById(R.id.select_bodyTextView);
+		handButton.setOnClickListener(this);
+		legButton.setOnClickListener(this);
+		bodyButton.setOnClickListener(this);
+
+	}
+	
 	@Override
 	protected void onResume() {
 		// TODO Auto-generated method stub
@@ -31,6 +50,34 @@ public class SelectSportActivity extends Activity{
 		// TODO Auto-generated method stub
 		super.onDestroy();
 	}
-
 	
+	private void goPage(Class<?> cls){
+		intent = new Intent();
+		intent.setClass(this, cls);
+		startActivity(intent);
+	}
+
+	@Override
+	public void onClick(View view) {
+		switch (view.getId()) {
+		case R.id.select_handTextView:
+
+			goPage(HandSportActivity.class);
+			break;
+
+		case R.id.select_legTextView:
+
+			goPage(LegSportActivity.class);
+			break;
+			
+		case R.id.select_bodyTextView:
+
+			goPage(BodySportActivity.class);
+			break;
+
+		default:
+			break;
+		}
+	}
+
 }
